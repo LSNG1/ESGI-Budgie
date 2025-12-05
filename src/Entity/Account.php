daee33b0d571b9980757d9009892391206f92856
LSNG <67861281+LSNG1@users.noreply.github.com>
2025-12-05 11:53:39 +0100

    feat: Add Account entity and repository for account management

---
+++ src/Entity/Account.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccountRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AccountRepository::class)]
+class Account
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 4)]
+    private ?string $taxRate = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 6, scale: 4)]
+    private ?string $rateOfPay = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 15, scale: 4)]
+    private ?string $overdraft = null;
+
+    #[ORM\Column]
+    private ?\DateTime $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTaxRate(): ?string
+    {
+        return $this->taxRate;
+    }
+
+    public function setTaxRate(string $taxRate): static
+    {
+        $this->taxRate = $taxRate;
+
+        return $this;
+    }
+
+    public function getRateOfPay(): ?string
+    {
+        return $this->rateOfPay;
+    }
+
+    public function setRateOfPay(string $rateOfPay): static
+    {
+        $this->rateOfPay = $rateOfPay;
+
+        return $this;
+    }
+
+    public function getOverdraft(): ?string
+    {
+        return $this->overdraft;
+    }
+
+    public function setOverdraft(string $overdraft): static
+    {
+        $this->overdraft = $overdraft;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
