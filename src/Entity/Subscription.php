53326e2d8d1df3672e72bd485e09262355ba4637
LSNG <67861281+LSNG1@users.noreply.github.com>
2025-12-05 12:18:35 +0100

    fix: Downgrade symfony/serializer to version 7.4 and update related dependencies in composer files

--- src/Entity/Subscription.php
+++ src/Entity/Subscription.php
@@ -5,26 +5,40 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\SubscriptionRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: SubscriptionRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Get()
+    ],
+    normalizationContext: ['groups' => ['subscription:read']],
+)]
 class Subscription
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['subscription:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 100)]
+    #[Groups(['subscription:read'])]
     private ?string $name = null;
 
     #[ORM\Column(nullable: true)]
+    #[Groups(['subscription:read'])]
     private ?int $maxAccounts = null;
 
     #[ORM\Column(nullable: true)]
+    #[Groups(['subscription:read'])]
     private ?int $maxIncomes = null;
 
     #[ORM\Column(nullable: true)]
+    #[Groups(['subscription:read'])]
     private ?int $maxExpenses = null;
 
     public function getId(): ?int
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,21 +2,29 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['user:read']],
+    denormalizationContext: ['groups' => ['user:write']],
+)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[Groups(['user:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['user:read','user:write'])]
     private ?string $email = null;
 
     /**
@@ -32,10 +40,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 100, nullable: true)]
+    #[Groups(['user:read','user:write'])]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 100, nullable: true)]
+    #[Groups(['user:read','user:write'])]
     private ?string $lastname = null;
+
 
     #[ORM\Column(length: 30, nullable: true)]
     private ?string $phone = null;
