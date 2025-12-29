bbb6baad07d61993ef4eaf6fc66f48575b6dc7be
Wilfried Bour <wilfried.bourguignon@epitech.eu>
2025-12-29 23:11:32 +0100

    Update User entity

--- server/src/Entity/User.php
+++ server/src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
 
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
@@ -38,6 +39,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Groups(['user:write', 'user:read'])]
     private ?string $password = null;
 
     #[ORM\Column(length: 100, nullable: true)]
@@ -48,14 +50,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[Groups(['user:read','user:write'])]
     private ?string $lastname = null;
 
-
     #[ORM\Column(length: 30, nullable: true)]
+    #[Groups(['user:read','user:write'])]
     private ?string $phone = null;
 
     #[ORM\Column(length: 50, nullable: true)]
+    #[Groups(['user:read','user:write'])]
     private ?string $fiscalNum = null;
 
     #[ORM\Column]
+    #[Groups(['user:read','user:write'])]
     private ?bool $verified = null;
 
     #[ORM\ManyToOne]
